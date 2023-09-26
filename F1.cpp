669a516706d22cf2c9ffb8cb69080114daf4da70
oscarfuentes22 <97373140+oscarfuentes22@users.noreply.github.com>
2023-09-26 07:36:39 -0600

    sort y structure

--- F1.cpp
+++ F1.cpp
@@ -9,16 +9,36 @@
 #include <ctime>
 
 using namespace std;
-
-int randomNumber(){
-    return std::rand() % 100 + 1;
-}
+struct Car {
+    char name[10];
+    float currentSpeed;
+    float originalSpeed;
+    int tireType; // 0: soft, 1: medium, 2: hard
+    int lapCount;
+    int lapsBeforePitstop;
+    float lapTime;
+    float totalTime;
+};
 
 int main(int argc, char const *argv[])
 {
     std::srand(std::time(0));
     std::cout << "Numero aleatorio: " << randomNumber() << std::endl;
+    
+    std::vector<Car> cars = /* inicializa tu vector de coches aquí */;
+    
+    std::sort(cars.begin(), cars.end(), compareCars);
+
+    for (const auto& car : cars) {
+        std::cout << car.name << ", Total Time: " << car.totalTime << std::endl;
+    }
 
     return 0;
+}
+int randomNumber(){
+    return std::rand() % 100 + 1;
+}
+bool compareCars(const Car& a, const Car& b) {
+    return a.totalTime < b.totalTime;
 }
 
